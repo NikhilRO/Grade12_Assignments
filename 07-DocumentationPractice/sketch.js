d0f61191c928f3ec91fe1e605f3d5ca3b5efea16
Nikhil Gupta <845031@pdsb.org>
2016-10-03 13:18:50 -0400

    Added additional documentation

--- 07-DocumentationPractice/sketch.js
+++ 07-DocumentationPractice/sketch.js
@@ -20,6 +20,8 @@ function draw() {
   textDisplay();
 }
 
+/** This function displays the instruction to perform all the operations
+ */
 function textDisplay() {
   fill(255, 200);
   textSize(16);
@@ -37,7 +39,13 @@ function textDisplay() {
   conditionals();
 }
 
+/** This function displays text according to what operation is being performed
+ *
+ *  @param {string} stri   This is the string to display the array
+ *  @param {string} stri2  This is the string to display the reversed array
+ */
 function conditionals() {
+
   if (additionB) {
     text(addition(mouseX, mouseY), mouseX, mouseY);
   }
@@ -66,162 +74,164 @@ function conditionals() {
     text(stri, windowWidth * (1 / 2 + 1 / 4 - 1 / 8), 50);
   }
   if (reverseArrayB) {
-    var stri = "{";
-    for (var i = 0; i < mouseValues.length; i++) {
-      stri += " " + mouseValues[i] + " ";
+    var stri2 = "{";
+    for (var j = 0; j < mouseValues.length; j++) {
+      stri2 += " " + mouseValues[j] + " ";
     }
-    stri += "}";
+    stri2 += "}";
     textAlign(CENTER);
-    text(stri, windowWidth * (1 / 2 + 1 / 4 - 1 / 8), 50);
+    text(stri2, windowWidth * (1 / 2 + 1 / 4 - 1 / 8), 50);
   }
+}
 
-  function mousePressed() {
-    if (creatingArrayB) {
-      mouseValues.push(subtract(mouseX, mouseY));
-    }
+function mousePressed() {
+  if (creatingArrayB) {
+    mouseValues.push(subtract(mouseX, mouseY));
   }
+}
 
-  function keyTyped() {
-    creatingArrayB = false;
-    additionB = false;
-    subtractB = false;
-    multiplyB = false;
-    divideB = false;
-    maximumB = false;
-    minimumB = false;
-    reverseArrayB = false;
-    if (key === 'A' || key === 'a') {
-      additionB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'S' || key === 's') {
-      subtractB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'M' || key === 'm') {
-      multiplyB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'D' || key === 'd') {
-      divideB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'X' || key === 'x') {
-      maximumB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'N' || key === 'n') {
-      minimumB = true;
-      mouseValues.splice(0, mouseValues.length - 1);
-    }
-    if (key === 'c' || key === 'C') {
-      creatingArrayB = true;
-    }
-    if (key === 'r' || key === 'R') {
-      reverseArray(mouseValues);
-      reverseArrayB = true;
-    }
+function keyTyped() {
+  creatingArrayB = false;
+  additionB = false;
+  subtractB = false;
+  multiplyB = false;
+  divideB = false;
+  maximumB = false;
+  minimumB = false;
+  reverseArrayB = false;
+  if (key === 'A' || key === 'a') {
+    additionB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-  /** This function takes in two float values and returns the sum of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The sum of first and second as a float value
-   */
-  function addition(first, second) {
-    return first + second;
+  if (key === 'S' || key === 's') {
+    subtractB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-
-  /** This function takes in two float values and returns the difference of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The difference of first and second as a float value
-   */
-  function subtract(first, second) {
-    return first - second;
+  if (key === 'M' || key === 'm') {
+    multiplyB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-
-  /** This function takes in two float values and returns the product of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The product of first and second as a float value
-   */
-  function multiply(first, second) {
-    return first * second;
+  if (key === 'D' || key === 'd') {
+    divideB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-
-  /** This function takes in two float values and returns the result of dividing of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The result of dividing of first and second as a float value
-   */
-  function divide(first, second) {
-    if (second === 0) {
-      return "not defined";
-    } else {
-      return first / second;
-    }
+  if (key === 'X' || key === 'x') {
+    maximumB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-
-  /** This function takes in two float values and returns the bigger of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The bigger of first and second as a float value
-   */
-  function maximum(first, second) {
-    if (first > second) {
-      return first;
-    } else if (first < second) {
-      return second;
-    } else {
-      return "equal";
-    }
+  if (key === 'N' || key === 'n') {
+    minimumB = true;
+    mouseValues.splice(0, mouseValues.length - 1);
   }
-
-  /** This function takes in two float values and returns the smaller of those two values
-   *
-   *  @param {number} first   This is the first float of the sum
-   *  @param {number} second  This is the second float of the sum
-   *  @return {number}        The smaller of first and second as a float value
-   */
-  function minimum(first, second) {
-    if (first < second) {
-      return first;
-    } else if (first > second) {
-      return second;
-    } else {
-      return "equal";
-    }
+  if (key === 'c' || key === 'C') {
+    creatingArrayB = true;
   }
+  if (key === 'r' || key === 'R') {
+    reverseArray(mouseValues);
+    reverseArrayB = true;
+  }
+}
 
-  /** This function takes in an array and reverses the order of the array
-   *
-   *  @param {array} temp   This is a temporary array used to store the reversed input array
-   *  @return {array}       The input array with its elements' orders reversed
-   */
-  function reverseArray(first) {
-    var temp = [];
+/** This function takes in two float values and returns the sum of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The sum of first and second as a float value
+ */
+function addition(first, second) {
+  return first + second;
+}
 
-    for (var i = first.length - 1; i >= 0; i--) {
-      temp.push(first[i]);
-    }
+/** This function takes in two float values and returns the difference of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The difference of first and second as a float value
+ */
+function subtract(first, second) {
+  return first - second;
+}
 
-    for (var j = 0; j < temp.length; j++) {
-      first[j] = temp[j];
-    }
+/** This function takes in two float values and returns the product of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The product of first and second as a float value
+ */
+function multiply(first, second) {
+  return first * second;
+}
 
+/** This function takes in two float values and returns the result of dividing of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The result of dividing of first and second as a float value
+ */
+function divide(first, second) {
+  if (second === 0) {
+    return "not defined";
+  } else {
+    return first / second;
+  }
+}
+
+/** This function takes in two float values and returns the bigger of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The bigger of first and second as a float value
+ */
+function maximum(first, second) {
+  if (first > second) {
     return first;
+  } else if (first < second) {
+    return second;
+  } else {
+    return "equal";
+  }
+}
+
+/** This function takes in two float values and returns the smaller of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The smaller of first and second as a float value
+ */
+function minimum(first, second) {
+  if (first < second) {
+    return first;
+  } else if (first > second) {
+    return second;
+  } else {
+    return "equal";
+  }
+}
+
+/** This function takes in an array and reverses the order of the array
+ *
+ *  @param {array} temp   This is a temporary array used to store the reversed input array
+ *  @return {array}       The input array with its elements' orders reversed
+ */
+function reverseArray(first) {
+  var temp = [];
+
+  for (var i = first.length - 1; i >= 0; i--) {
+    temp.push(first[i]);
   }
 
-  /*     
-  text("{", mouseX - mouseX / 2 - 40, mouseY);
-      for (var j = 0; j < mouseValues.length; j++) { // why can't i use "i"?
-        text(mouseValues[j], mouseX - mouseX / 2 + 40 * j, mouseY);
-      }
-      text("}", mouseX - mouseX / 2 + 40 * mouseValues.length, mouseY);
+  for (var j = 0; j < temp.length; j++) {
+    first[j] = temp[j];
+  }
+
+  return first;
+}
+
+/*     
+text("{", mouseX - mouseX / 2 - 40, mouseY);
+    for (var j = 0; j < mouseValues.length; j++) { // why can't i use "i"? // BECAUSE THE SCOPE OF THE VARIABLE IS THE ENTIRE FUNCTION
+      text(mouseValues[j], mouseX - mouseX / 2 + 40 * j, mouseY);
     }
-    */
+    text("}", mouseX - mouseX / 2 + 40 * mouseValues.length, mouseY);
+  }
+  */
