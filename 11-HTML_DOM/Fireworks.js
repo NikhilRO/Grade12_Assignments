080c6e36d8b14395c7604aa75fcec7ddf2e316bb
Nikhil Gupta <845031@pdsb.org>
2016-10-24 13:47:23 -0400

    Started New Assignment

---
+++ 11-HTML_DOM/Fireworks.js
@@ -0,0 +1,46 @@
+function Fireworks(location, endLocation, radius, moveVector, attracted) {
+  this.velocity = -100;
+  this.endLocation = endLocation;
+  this.location = location;
+  this.gravity = createVector(0, 0.00098);
+  this.radius = radius;
+  this.moveVector = moveVector;
+  console.log(moveVector);
+  this.attracted = attracted || false;
+  this.time = 0;
+  this.attractiveForce;
+  this.distance;
+
+  this.display = function() {
+    fill(255, 0, 0);
+    stroke(255,0,0);
+    strokeWeight(2);
+    
+    ellipse(this.location.x, this.location.y, 2 * this.radius, 2 * this.radius);
+  }
+
+  this.move = function() {
+    this.time++;
+
+    if (this.attracted && (this.time >= 50)) {
+      this.attractiveForce = endLocation.copy();
+      this.attractiveForce.sub(location);
+
+      this.distance = this.attractiveForce.mag();
+      this.distance = constrain(this.distance, 1, 50);
+
+      this.attractiveForce.normalize();
+      this.attractiveForce.mult(this.distance / 10);
+
+      this.location.add(this.attractiveForce);
+    } else {
+      this.location.add(this.moveVector);
+      this.moveVector.add(this.gravity);
+    }
+  }
+
+  this.run = function() {
+    this.display()
+    this.move();
+  }
+}
---
+++ 11-HTML_DOM/sketch.js
@@ -0,0 +1,84 @@
+var startBlast, loadDestinations, explosion, shooterCircle;
+var finalDestinations = [];
+var fire = [];
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  startBlast = false;
+  loadDestinations = false;
+  explosion = false;
+
+  shooterCircle = new Fireworks(createVector(width / 2, height), createVector(width / 2, (1 / 4) * height), 10, createVector(0, -5));
+}
+
+function draw() {
+  background(0,50);
+  destinations();
+  firework();
+}
+
+function mouseDragged() {
+  if (!startBlast && !explosion) {
+    loadDestinations = true;
+  }
+}
+
+function mouseReleased() {
+  if (!startBlast && !explosion) {
+    loadDestinations = false;
+  }
+}
+
+function destinations() {
+  if (loadDestinations) {
+    finalDestinations.push(createVector(mouseX, mouseY)); //how to push vectors into them
+  }
+}
+
+function keyTyped() {
+  if (!loadDestinations) {
+    if (key === "B" || key === "b") { //Is this okay?
+      startBlast = true;
+    }
+    if (key === "R" || key === "r") {
+      reset();
+    }
+  }
+}
+
+function firework() {
+  if (startBlast) {
+    shooterCircle.run();
+
+    if (shooterCircle.location.y < (1 / 4) * height) {
+      explosion = true;
+      startBlast = false;
+      //console.log(shooterCircle.location);
+      for (var i = 0; i < finalDestinations.length; i++) {
+        fire.push(new Fireworks(shooterCircle.location.copy(), finalDestinations[i], 1.3, (p5.Vector.random2D()).mult(5) ,true));
+      }
+      shooterCircle = undefined;
+    }
+  }
+
+  if (explosion) {
+    for (var j = 0; j < fire.length; j++) {
+      fire[j].run();
+    }
+    // if (frameCount % 10 === 1) {
+    //   noLoop();
+    // }
+  }
+}
+
+
+function reset() {
+  finalDestinations.splice(0, finalDestinations.length);
+  startBlast = false;
+  loadDestinations = false;
+  explosion = false;
+
+  shooterCircle = new Fireworks(createVector(width / 2, height), createVector(width / 2, (1 / 4) * height), 10, createVector(0, -5));
+}
