b972f1cfa446e8ad16cc204396c917d783ae0c23
Nikhil Gupta <845031@pdsb.org>
2016-10-26 14:30:15 -0400

    loaded text file

--- 12-TextIO-Gutenberg/sketch.js
+++ 12-TextIO-Gutenberg/sketch.js
@@ -1,7 +1,14 @@
+var story= [];
+function preload() {
+  story = loadStrings('rhesus.txt');
+}
+
 function setup() {
-  
+    console.log(story.length);
 }
 
 function draw() {
-  
+  //ellipse(width/2, height/2, 50, 50);
 }
+
+// .split .toChar
