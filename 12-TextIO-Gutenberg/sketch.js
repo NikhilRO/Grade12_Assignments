707fb086192911a134ad509dbe94dfadbcce76ac
Nikhil Gupta <niks.gupta09@gmail.com>
2016-10-27 12:06:47 -0400

    working on the program

--- 12-TextIO-Gutenberg/sketch.js
+++ 12-TextIO-Gutenberg/sketch.js
@@ -1,18 +1,35 @@
 var story = [];
+var storyWords = [];
+var previousWidth, lineNumber;
 var img;
 
 function preload() {
-  //story = loadStrings('rhesus.txt');
+  story = loadStrings('rhesus.txt');
   img = loadImage("image.jpeg");
+  storyWords = split(story[10]," ");
 }
 
 function setup() {
-  // console.log(story.length);
+  previousWidth = 0
+  lineNumber = 0;
+
+  console.log(storyWords.length);
   createCanvas(windowWidth, windowHeight);
+
 
   image(img, 0, 0, windowWidth, windowHeight);
 
-  noCursor();
+  for (var i = 0; i < storyWords.length; i++) {
+    text(storyWords[i], previousWidth, lineNumber * 10);
+    previousWidth += textWidth(storyWords[i]) + 2;
+    if (previousWidth > windowWidth) { //CAN I USE WIDTH?
+      previousWidth = 0;
+      lineNumber++;
+    }
+  }
+
+ // noCursor();
+
 }
 
 function draw() {
