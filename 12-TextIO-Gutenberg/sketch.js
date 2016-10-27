7c013fd2e32953c37ab5cf56be479b468f3cb420
Nikhil Gupta <845031@pdsb.org>
2016-10-27 13:27:10 -0400

    used words, characters

--- 12-TextIO-Gutenberg/sketch.js
+++ 12-TextIO-Gutenberg/sketch.js
@@ -1,5 +1,6 @@
 var story = [];
-var storyWords;
+var storyWords = [];
+var storyChar = [];
 var previousWidth, lineNumber;
 var img;
 
@@ -11,31 +12,50 @@ function preload() {
 function setup() {
   previousWidth = 0
   lineNumber = 0;
+  for (var j = 500; j < story.length - 100; j++) {
+    var tempArray = splitTokens(story[j], " -!*_,.");
+    for (var k = 0; k < tempArray.length; k++) {
+      storyWords.push(tempArray[k].toLowerCase());
+    }
+  }
 
-  storyWords.push(split(story[i]," ");
+  for (var l = 100; l < storyWords.length; l++) {
+    var tempArray2 = split(storyWords[l], "");
+    for (var m = 0; m < tempArray2.length; m++) {
+        storyChar.push(tempArray2[m].toLowerCase());
+    }
+  }
+
+
+  console.log(story.length);
   console.log(storyWords.length);
+  console.log(storyChar.length);
 
-  //console.log(storyWords.length);
   createCanvas(windowWidth, windowHeight);
 
+  //image(img, 0, 0, windowWidth, windowHeight);
 
-  image(img, 0, 0, windowWidth, windowHeight);
-
-  for (var i = 0; i < storyWords.length; i++) {
-    text(storyWords[i], previousWidth, lineNumber * 10);
-    previousWidth += textWidth(storyWords[i]) + 2;
+  // for (var i = 0; i < storyWords.length; i++) {
+  //   textSize(8);
+  //   text(storyWords[i], previousWidth, lineNumber * 8);
+  //   previousWidth += textWidth(storyWords[i]) + 2;
+  //   if (previousWidth > windowWidth) { //CAN I USE WIDTH?
+  //     previousWidth = 0;
+  //     lineNumber++;
+  //   }
+  // }
+  for (var i = 0; i < storyChar.length; i++) {
+    textSize(8);
+    text(storyChar[i], previousWidth, lineNumber * 8);
+    previousWidth += textWidth(storyChar[i]) + 12;
     if (previousWidth > windowWidth) { //CAN I USE WIDTH?
       previousWidth = 0;
       lineNumber++;
     }
   }
 
- // noCursor();
+  // noCursor();
 
 }
 
-function draw() {
-  //ellipse(width/2, height/2, 50, 50);
-}
-
-// .split .toChar
+function draw() {}
