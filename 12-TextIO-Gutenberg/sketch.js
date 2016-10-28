eea4283e93d10dc14ce8e50f2a83c35ca6ebf5b0
Nikhil Gupta <845031@pdsb.org>
2016-10-28 14:50:03 -0400

    working on image pixels

--- 12-TextIO-Gutenberg/sketch.js
+++ 12-TextIO-Gutenberg/sketch.js
@@ -1,8 +1,9 @@
 var story = [];
 var storyWords = [];
 var storyChar = [];
+var colImage = [];
 var previousWidth, lineNumber;
-var img;
+var img, bPrev;
 
 function preload() {
   story = loadStrings('rhesus.txt');
@@ -12,8 +13,9 @@ function preload() {
 function setup() {
   previousWidth = 0
   lineNumber = 0;
+  bPrev = 0;
   for (var j = 500; j < story.length - 100; j++) {
-    var tempArray = splitTokens(story[j], " -!*_,.");
+    var tempArray = splitTokens(story[j], " \-\?!*_,.");
     for (var k = 0; k < tempArray.length; k++) {
       storyWords.push(tempArray[k].toLowerCase());
     }
@@ -22,7 +24,7 @@ function setup() {
   for (var l = 100; l < storyWords.length; l++) {
     var tempArray2 = split(storyWords[l], "");
     for (var m = 0; m < tempArray2.length; m++) {
-        storyChar.push(tempArray2[m].toLowerCase());
+      storyChar.push(tempArray2[m].toLowerCase());
     }
   }
 
@@ -33,29 +35,46 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
-  //image(img, 0, 0, windowWidth, windowHeight);
+  image(img, 0, 0, windowWidth, windowHeight);
+  img.resize(windowWidth, windowHeight);
+  img.loadPixels();
+  for (var a = 0; a < img.height; a += 8) {
+    for (var b = 0; b < img.width; b += storyChar[a + bPrev].length) {
+      colImage.push(color(img.pixels[b + a * width].red, img.pixels[b + a * width].green, img.pixels[b + a * width].blue));
+      bPrev++;
+    }
+    bPrev = 0;
+  }
+  img.updatePixels();
+  console.log(colImage.length);
 
-  // for (var i = 0; i < storyWords.length; i++) {
-  //   textSize(8);
-  //   text(storyWords[i], previousWidth, lineNumber * 8);
-  //   previousWidth += textWidth(storyWords[i]) + 2;
-  //   if (previousWidth > windowWidth) { //CAN I USE WIDTH?
-  //     previousWidth = 0;
-  //     lineNumber++;
-  //   }
-  // }
-  for (var i = 0; i < storyChar.length; i++) {
+  for (var i = 0; i < colImage.length; i++) {
+    stroke(colImage[i]);
     textSize(8);
     text(storyChar[i], previousWidth, lineNumber * 8);
-    previousWidth += textWidth(storyChar[i]) + 12;
+    previousWidth += textWidth(storyChar[i]) + 1;
     if (previousWidth > windowWidth) { //CAN I USE WIDTH?
       previousWidth = 0;
       lineNumber++;
     }
   }
 
+
   // noCursor();
 
 }
 
 function draw() {}
+
+/* Comment of self
+//prints words
+  for (var i = 0; i < storyWords.length; i++) {
+    textSize(8);
+    text(storyWords[i], previousWidth, lineNumber * 8);
+    previousWidth += textWidth(storyWords[i]) + 2;
+    if (previousWidth > windowWidth) { //CAN I USE WIDTH?
+      previousWidth = 0;
+      lineNumber++;
+    }
+  }
+  */
