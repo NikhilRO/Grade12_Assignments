be8e17caada77b980b45ba2732dfea327e3a60e0
Nikhil Gupta <845031@pdsb.org>
2016-11-11 12:36:30 -0500

    changed console.log to changeP

--- 13-Text-RegularExpressions/sketch.js
+++ 13-Text-RegularExpressions/sketch.js
@@ -8,15 +8,15 @@ function preload() {
 function setup() {
   previousWidth = 0
   lineNumber = 0;
+  
+  noCanvas();
 
-  console.log("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
-  console.log("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
-  console.log("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
-  console.log("Italics: " + block.match(/\b\_.*?\_\b/g).length);
-  console.log("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length+ block.match(/\./g).length));
+  createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
+  createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
+  createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
+  createP("Italics: " + block.match(/\b\_.*?\_\b/g).length);
+  createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length+ block.match(/\./g).length));
 }
-
-function draw() {} //Do I even need draw??
 
 /** 
  * This function takes the array of text/story and converts it one big block/string 
