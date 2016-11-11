e8012866609837cd9fca549ce389f6d83d026a42
Nikhil Gupta <niks.gupta09@gmail.com>
2016-11-10 23:06:29 -0500

    Completed the basics

--- 13-Text-RegularExpressions/sketch.js
+++ 13-Text-RegularExpressions/sketch.js
@@ -1,5 +1,5 @@
 var story = [];
-var previousWidth, lineNumber, block;
+var block;
 
 function preload() {
   story = loadStrings('rhesus.txt', lineToBlock);
@@ -13,13 +13,10 @@ function setup() {
   console.log("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
   console.log("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
   console.log("Italics: " + block.match(/\b\_.*?\_\b/g).length);
-  console.log("Symbols: " + block.match(/\b\_.*?\_\b/g).length);
-  
-
-  // console.log(story.length);
+  console.log("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length+ block.match(/\./g).length));
 }
 
-function draw() {}
+function draw() {} //Do I even need draw??
 
 /** 
  * This function takes the array of text/story and converts it one big block/string 
@@ -29,9 +26,3 @@ function lineToBlock() {
     block = block + " " + story[i];
   }
 }
-
-
-
-
-/* Comment of self
- */
