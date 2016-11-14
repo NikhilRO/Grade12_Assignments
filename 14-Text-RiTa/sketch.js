26a10724a1fb8e0df24e9a9be132bfca7f757034
Nikhil Gupta <845031@pdsb.org>
2016-11-14 14:04:43 -0500

    Started new assignment

---
+++ 14-Text-RiTa/sketch.js
@@ -0,0 +1,28 @@
+var story = [];
+var block;
+
+function preload() {
+  story = loadStrings('rhesus.txt', lineToBlock);
+}
+
+function setup() {
+  previousWidth = 0
+  lineNumber = 0;
+  
+  noCanvas();
+
+  createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
+  createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
+  createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
+  createP("Italics: " + block.match(/\b\_.*?\_\b/g).length);
+  createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length+ block.match(/\./g).length));
+}
+
+/** 
+ * This function takes the array of text/story and converts it one big block/string 
+ */
+function lineToBlock() {
+  for (var i = 0; i < story.length; i++) {
+    block = block + " " + story[i];
+  }
+}
