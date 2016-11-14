a4473a0fb784cb350d580638d04a94a78088f9eb
Nikhil Gupta <845031@pdsb.org>
2016-11-14 14:45:24 -0500

    minor changes

--- 14-Text-RiTa/sketch.js
+++ 14-Text-RiTa/sketch.js
@@ -1,5 +1,5 @@
 var story = [];
-var block;
+var block, ritaStory;
 
 function preload() {
   story = loadStrings('rhesus.txt', lineToBlock);
@@ -8,19 +8,23 @@ function preload() {
 function setup() {
   previousWidth = 0
   lineNumber = 0;
-  
+
   noCanvas();
 
   createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
   createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
   createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
   createP("Italics: " + block.match(/\b\_.*?\_\b/g).length);
-  createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length+ block.match(/\./g).length));
+  createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length + block.match(/\./g).length));
+  
+  
 }
 
 /** 
  * This function takes the array of text/story and converts it one big block/string 
  */
 function lineToBlock() {
-block= story.join("\n"); // \n creates a new line 
+  block = story.join("\n"); // \n creates a new line 
+  ritaStory = RiString(block);
+  
 }
