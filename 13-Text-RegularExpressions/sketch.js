56712b7c6eec6776a81b3980ec0d8ab28bfb78d9
Nikhil Gupta <niks.gupta09@gmail.com>
2016-11-21 14:37:37 -0500

    minor changes

--- 13-Text-RegularExpressions/sketch.js
+++ 13-Text-RegularExpressions/sketch.js
@@ -7,15 +7,12 @@ function preload() {
 }
 
 function setup() {
-  previousWidth = 0
-  lineNumber = 0;
-
   noCanvas();
 
   createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
   createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
   createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
-  
+
   var italics = block.match(/\b\_.*?\_\b/g);
   for (var i = 0; i < italics.length; i++) {
     var tempArray = split(italics[i], " ");
@@ -30,8 +27,8 @@ function setup() {
   createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length + block.match(/\./g).length));
 }
 
-/** 
- * This function takes the array of text/story and converts it one big block/string 
+/**
+ * This function takes the array of text/story and converts it one big block/string
  */
 function lineToBlock() {
   for (var i = 0; i < story.length; i++) {
--- 14-Text-RiTa/sketch.js
+++ 14-Text-RiTa/sketch.js
@@ -6,20 +6,16 @@ function preload() {
 }
 
 function setup() {
-  previousWidth = 0
-  lineNumber = 0;
-
   noCanvas();
 
   createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
   createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
   createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
-  createP("Italics: " + block.match(/\b\_.*?\_\b/g).length);
   createP("Symbols: " + (block.match(/\?/g).length + block.match(/\!/g).length + block.match(/\./g).length));
   createP("Nouns: " + (ritaPosJoined.match(/\bnn\b/g).length + ritaPosJoined.match(/\bnnp\b/g).length));
   createP("Verbs: " + ritaPosJoined.match(/\bvb.*?\b/g).length);
   createP("Pronouns: " + (ritaPosJoined.match(/\bprp.*\b/g).length + ritaPosJoined.match(/\bwp.*\b/g).length));
-} 
+}
 
 /**
  * This function takes the array of text/story and converts it into one big block/string. Then it applies several rita functions to get a string of Part of Speech
