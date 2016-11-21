28168032c84daa1724c04b8d18ce11e9c0adbf7e
Nikhil Gupta <niks.gupta09@gmail.com>
2016-11-21 14:44:43 -0500

    minor changes

--- 15-Text-SortingAlgorithms/sketch.js
+++ 15-Text-SortingAlgorithms/sketch.js
@@ -12,19 +12,19 @@ function setup() {
   createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
   createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
   createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
-  
+
 }
 
 /**
- * This function takes the array of text/story and converts it into one big block/string. Then it applies several rita functions to get a string of Part of Speech
+ * This function takes the array of text/story and converts it into one big block/string. Then it applies several rita functions to it.
  */
 function lineToBlock() {
   block = story.join("\n"); // \n creates a new line
   ritaStory = RiString(block);
 
   storyWords = ritaStory.words();
-
   saveStrings(storyWords.join("\n") ,'words.txt');
+
 
   ritaPos = ritaStory.pos();
   ritaPosJoined = ritaPos.join("\n");
