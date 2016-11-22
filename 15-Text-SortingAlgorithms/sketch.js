e736d7d01491a3d863f63c0bd3b99516ec0a083c
Nikhil Gupta <niks.gupta09@gmail.com>
2016-11-21 21:52:07 -0500

    basic layout for text to word and number array

--- 15-Text-SortingAlgorithms/sketch.js
+++ 15-Text-SortingAlgorithms/sketch.js
@@ -1,31 +1,27 @@
 var story = [];
-var block, ritaStory, ritaPos, pronoun, noun, verb, ritaPosJoined, storyWords, storyNumbers;
+var storyWords= [];
+var storyNumbers= [];
 
 function preload() {
-  story = loadStrings('rhesus.txt', lineToBlock);
+  story = loadStrings('rhesus.txt', lineToWord);
 }
 
 function setup() {
-
   noCanvas();
 
-  createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
-  createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
-  createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
+  // createP("Two Digit Numbers: " + block.match(/\b\d{2}\b/g).length);
+  // createP("Three Digit Numbers: " + block.match(/\b\d{3}\b/g).length);
+  // createP("Four Digit Numbers: " + block.match(/\b\d{4}\b/g).length);
 
 }
 
 /**
  * This function takes the array of text/story and converts it into one big block/string. Then it applies several rita functions to it.
  */
-function lineToBlock() {
-  block = story.join("\n"); // \n creates a new line
-  ritaStory = RiString(block);
-
-  storyWords = ritaStory.words();
-  saveStrings(storyWords.join("\n") ,'words.txt');
-
-
-  ritaPos = ritaStory.pos();
-  ritaPosJoined = ritaPos.join("\n");
+function lineToWord() {
+  storyWords = split(story.join(" ").toLowerCase() ,/[\d\W\_]+/);
+  console.log(storyWords.length)
+  storyNumbers= split(story.join(" ") ,/[\D\_]+/);
+  console.log(storyNumbers.length)
+ //saveStrings(storyWords, "words.txt");
 }
