a4fb1e3c472060f050044595cd88f0d5bb0a30e8
Nikhil Gupta <845031@pdsb.org>
2016-12-05 13:16:28 -0500

    basic setup done

---
+++ 16-Text-APIs(Wordnik)/Word.js
@@ -0,0 +1,6 @@
+function Word(word, relatedWords, phrases, definitions){
+  this.word= word;
+  this.definition= definitions;
+  this.relatedWord = relatedWords;
+  this.phrases= phrases;
+}
--- 16-Text-APIs(Wordnik)/sketch.js
+++ 16-Text-APIs(Wordnik)/sketch.js
@@ -1,6 +1,27 @@
+var englishWords, relatedWords, phrases, definitions;
+var words = [];
+
+function preload() {
+  var urlStart = "http://api.wordnik.com:80/v4/word.json/";
+  var englishWords = ["color", "developer", "math", "computer", "cumbersome", "mouse", "vicissitude", "sweet", "spectacle", "vivid"];
+  var urlRelatedWords= "/relatedWords?useCanonical=false&limitPerRelationshipType=10&"
+  var urlPhrases= "/phrases?limit=5&wlmi=0&useCanonical=false&"
+  var urlDefinitions = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&";
+  var apiKey = "api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+
+  for (var i = 0; i < englishWords.length; i++) {
+    var tempRelatedWord= loadJSON(urlStart + englishWords[i] + urlRelatedWords + apiKey);
+    var tempPhrases= loadJSON(urlStart + englishWords[i] + urlPhrases + apiKey);
+    var tempDefinitions= loadJSON(urlStart + englishWords[i] + urlDefinitions + apiKey);
+    words.push(new Word(englishWords[i], tempRelatedWord, tempPhrases,tempDefinitions))
+  }
+}
+
 function setup() {
+
 }
 
 function draw() {
-  
+
+
 }
