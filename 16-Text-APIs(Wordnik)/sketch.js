6e1dfc222ec6a471817762f860747c50a3d298b6
Nikhil Gupta <niks.gupta09@gmail.com>
2016-12-05 23:12:06 -0500

    added my api key

--- 16-Text-APIs(Wordnik)/sketch.js
+++ 16-Text-APIs(Wordnik)/sketch.js
@@ -3,11 +3,14 @@ var words = [];
 
 function preload() {
   var urlStart = "http://api.wordnik.com:80/v4/word.json/";
+  
   var englishWords = ["color", "developer", "math", "computer", "cumbersome", "mouse", "vicissitude", "sweet", "spectacle", "vivid"];
+  
   var urlRelatedWords= "/relatedWords?useCanonical=false&limitPerRelationshipType=10&"
   var urlPhrases= "/phrases?limit=5&wlmi=0&useCanonical=false&"
   var urlDefinitions = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&";
-  var apiKey = "api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+  
+  var apiKey = "api_key=b15d7084e9f53e2e1f1133b19060909bd2e61f628abba11f6";
 
   for (var i = 0; i < englishWords.length; i++) {
     var tempRelatedWord= loadJSON(urlStart + englishWords[i] + urlRelatedWords + apiKey);
