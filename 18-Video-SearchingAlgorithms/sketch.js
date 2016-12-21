05b89fd65e47320b4cedb0297a166292b7ee0792
Nikhil Gupta <845031@pdsb.org>
2016-12-21 13:24:07 -0500

    working with assignment

--- 18-Video-SearchingAlgorithms/sketch.js
+++ 18-Video-SearchingAlgorithms/sketch.js
@@ -1,11 +1,13 @@
-var capture;
+var capture, cnv, sortedVideo;
+var videoPixel = [];
 // var pixR = [];
 // var pixG = [];
 // var pixB = [];
 // var pixA = [];
 
 function setup() {
-  createCanvas(640, 480);
+  cnv = createCanvas(320, 240);
+  //sortedVideo = createCanvas(32, 24);
   capture = createCapture(VIDEO);
   capture.size(32, 24);
   //capture.hide();   //hides the video feed
@@ -17,17 +19,66 @@ function draw() {
   background(255);
   capture.size(32, 24);
   capture.loadPixels();
-  var videoPixel = [];
-  videoPixel.push.apply(pixels, capture.pixels);
-  for (var y = 0; y < capture.height; y++) {
-    for (var x = 0; x < capture.width; x++) {
+
+  videoPixel.splice(0, videoPixel.length);
+  videoPixel.push.apply(videoPixel, capture.pixels);
+  videoPixel.sort(function(a, b) {
+    return a - b
+  });
+
+  for (var x = 0; x < 32; x++) {
+    for (var y = 0; y < 24; y++) {
       var index = (x + y * width) * 4;
-      videoPixel[index + 0] = x;
-      videoPixel[index + 1] = random(255);
-      videoPixel[index + 2] = y;
-      videoPixel[index + 3] = 255;
+      //stroke(0);
+      fill(videoPixel[index + 0],
+        videoPixel[index + 1],
+        videoPixel[index + 2],
+        videoPixel[index + 3]);
+      ellipse(x * 10, y * 10, 8, 8);
+      //var index = (x + y * width) * 4;
+      // pixels[index + 0] = videoPixel[index + 0];
+      // pixels[index + 1] = videoPixel[index + 1];
+      // pixels[index + 2] = videoPixel[index + 2];
+      // pixels[index + 3] = videoPixel[index + 3];
     }
   }
+  //updatePixels();
+  // console.log(sortedVideo);
+  // sortedVideo.loadPixels();
+  // console.log(sortedVideo.pixels);
+  // sortedVideo.pixels.splice(0, sortedVideo.length);
+  // sortedVideo.pixels.push(videoPixel);
+  // for (var y = 0; y < sortedVideo.height; y++) {
+  //   for (var x = 0; x < sortedVideo.width; x++) {
+  //     var index = (x + y * width) * 4;
+  //     sortedVideo.pixels[index + 0] = videoPixel[index + 0];
+  //     sortedVideo.pixels[index + 1] = videoPixel[index + 1];
+  //     sortedVideo.pixels[index + 2] = videoPixel[index + 2];
+  //     sortedVideo.pixels[index + 3] = videoPixel[index + 3];
+  //   }
+  // }
+  // sortedVideo.updatePixels();
+  //   for (var y = 0; y < sortedVideo.height; y++) {
+  //   for (var x = 0; x < sortedVideo.width; x++) {
+  //     var index = (x + y * width) * 4;
+  //     sortedVideo.pixels[index + 0] = video;
+  //     sortedVideo.pixels[index + 1] = random(255);
+  //     sortedVideo.pixels[index + 2] = y;
+  //     sortedVideo.pixels[index + 3] = 255;
+  //   }
+  // }
+
+
+
+  // for (var y = 0; y < capture.height; y++) {
+  //   for (var x = 0; x < capture.width; x++) {
+  //     var index = (x + y * width) * 4;
+  //     videoPixel[index + 0] = x;
+  //     videoPixel[index + 1] = random(255);
+  //     videoPixel[index + 2] = y;
+  //     videoPixel[index + 3] = 255;
+  //   }
+  // }
 
   // for (var x = 0; x < capture.width; x++) {
   //   for (var y = 0; y < capture.height; y++) {
@@ -49,9 +100,12 @@ function draw() {
   //     }
   //   }
   // }
-  capture.updatePixels();
+  //capture.updatePixels();
 }
 
+function keyPressed() {
+  noLoop();
+}
 /*
   img.loadPixels();
   for (var a = 0; a < img.height; a += 8) {
