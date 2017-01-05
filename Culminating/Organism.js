5c935dc2e034b00fdda0ac28206adba9bcc76487
Nikhil Gupta <niks.gupta09@gmail.com>
2017-01-04 21:56:05 -0500

    added files

---
+++ Culminating/Organism.js
@@ -0,0 +1,74 @@
+function Organism(obj) { //(dataProvider, dataset, scientificName, rank, author, specificEpithet, kingdom, phylum, clas, order, family, genus, species, iabinId, accepted, taxonomyTreeUrl, taxonPageUrl) {
+  this.dataProvider= obj.
+  this.dataset= obj.
+  this.scientificName= obj.
+  this.rank= obj.
+  this.author= obj.Author;
+  this.specificEpithet= obj.
+  this.kingdom= obj.
+  this.phylum= obj.
+  this.clas= obj.Class;
+  this.order= obj.
+  this.family= obj.
+  this.genus= obj.
+  this.species= obj.
+  this.iabinId= obj.
+  this.accepted= obj.Accepted;
+  this.taxonomyTreeUrl= obj.
+  this.taxonPageUrl= obj.
+
+  this.display = function() {
+
+  }
+
+}
+
+"Y"
+
+:
+"(Hudson) C. Agardh"
+
+:
+"Bryopsidophyceae"
+Data provider
+:
+"Catalogue of Life: 2007 Annual Checklist"
+Dataset
+:
+"AlgaeBase"
+Family
+:
+"Bryopsidaceae"
+Genus
+:
+"Bryopsis"
+IABIN ID
+:
+"148591"
+Kingdom
+:
+"Plantae"
+Order
+:
+"Bryopsidales"
+Phylum
+:
+"Chlorophyta"
+Rank
+:
+"species"
+Scientific name
+:
+"Bryopsis plumosa"
+Species
+:
+"Bryopsis plumosa"
+Specific epithet
+:
+"plumosa"
+Taxon page url
+:
+"http://200.144.189.73/portal/species/148591"
+Taxonomy tree url
+:
+"http://200.144.189.73/portal/species/browse/taxon/148591
