58e4eeb77e2a619cdaf4839dc7dd71cdb28aa190
Nikhil Gupta <niks.gupta09@gmail.com>
2017-01-04 23:44:21 -0500

    too slow
    ideally, it's perfect but since so much data; program can't take it

--- Culminating/sketch.js
+++ Culminating/sketch.js
@@ -1,14 +1,41 @@
 //(accessed through IABIN data portal, AlgaeBase, http://200.144.189.73/portal/datasets/resource/4)
 
 var table;
-var organisms= [];
+var organisms = [];
 
 function preload() {
   table = loadTable("taxonomy-search-1398436937824.csv", "csv", "header", pushToArray);
 }
 
+function pushToArray() {
+  for (var i = 0; i < countProps(table.getObject()); i++) {
+    //console.log[1];
+    organisms.push(new Organism(table.getObject()[i]));
+  }
+  table = undefined;
+  console.log(countProps(table.getObject()));
+  console.log("done array: "+ millis());
+}
+
 function setup() {
-  console.log(millis());
+console.log("Setup:"+ millis());
+}
+
+
+
+function draw() {}
+
+//FOR SOME REASON .LENGTH DOES NOT WORK ON table.getObject().length
+function countProps(obj) {
+    var count = 0;
+    for (var p in obj) {
+      obj.hasOwnProperty(p) && count++;
+    }
+    return count; 
+}
+
+/*
+  
   //count the columns
   print(table.getRowCount() + " total rows in table");
   print(table.getColumnCount() + " total columns in table");
@@ -24,13 +51,4 @@ function setup() {
   //     print(table.getString(r, c));
   //   }
   // }
-}
-
-function pushToArray() {
-  for (var i = 0; i < table.getObject().length; i++) {
-    organisms.push(new Organism(table.getObject()[i]));
-  }
-  table= undefined;
-}
-
-function draw() {}
+*/
